3c124699935154f4b04f5d69bfd6243cf4530213
Sahil Gandhi <sahilmgandhi@gmail.com>
2016-10-01 13:10:12 -0700

    Added Message Form

--- popup.js
+++ popup.js
@@ -5,20 +5,21 @@
 // Create a client instance
 
 // called when the client connects
-var url = '' //Global URL variable
+var url = ''; //Global URL variable
+
 function onConnect() {
   // Once a connection has been made, make a subscription and send a message.
   console.log('onConnect');
   getCurrentTabUrl(function(u) {
-    url = u
-    client.subscribe(url)
-    console.log('Subscribed to: ' + url)
+    url = u;
+    client.subscribe(url);
+    console.log('Subscribed to: ' + url);
   })
 
-  // client.subscribe("/World");
-  // message = new Paho.MQTT.Message("Hello");
-  // message.destinationName = "/World";
-  // client.send(message);
+  makeMessageForm(function (message)
+  {
+      sendMessage(message);
+  })
 }
 
 // called when the client loses its connection
@@ -31,12 +32,17 @@ function onConnectionLost(responseObject) {
 // called when a message arrives
 function onMessageArrived(message) {
   console.log('onMessageArrived: ' + message.payloadString);
+  var parent = document.getElementById('status');
+  var para = document.createElement("p");
+  var node = document.createTextNode(message.payloadString);
+  para.appendChild(node);
+  parent.appendChild(para);
 }
 
 function sendMessage(messageText) {
-  var message = new Paho.MQTT.Message(messageText)
-  message.destinationName = url
-  client.send(message)
+  var message = new Paho.MQTT.Message(messageText);
+  message.destinationName = url;
+  client.send(message);
 }
 
 /**
@@ -77,33 +83,38 @@ function renderStatus(statusText) {
   document.getElementById('status').textContent = statusText;
 }
 
+function makeMessageForm (callback)
+{
+  var parent = document.getElementById('status');
+  var writeMessageForm = document.createElement("form");
+  writeMessageForm.setAttribute('id', 'form');
+  var messageInput = document.createElement("input");
+  messageInput.setAttribute('type', 'text');
+  messageInput.setAttribute('id', 'message');
+  var messageSubmit = document.createElement("input");
+  messageSubmit.setAttribute('type', 'button');
+  messageSubmit.setAttribute('value', 'Submit');
+
+  messageSubmit.addEventListener('click', function() {
+    console.log("Submit button clicked");
+    var message = document.getElementById('message').value;
+    console.log("The message was: " + message);
+    document.getElementById('form').reset();
+    callback(message);
+    return false;
+  })
+  
+  writeMessageForm.appendChild(messageInput);
+  writeMessageForm.appendChild(messageSubmit);
+  // parent.appendChild(writeMessageForm);
+
+  document.getElementsByTagName('body')[0].appendChild(writeMessageForm);
+}
+
 document.addEventListener('DOMContentLoaded', function() {
-  client = new Paho.MQTT.Client('broker.hivemq.com', 8000, 'vansh');
+  client = new Paho.MQTT.Client('broker.hivemq.com', 8000, 'sahil');
   client.onConnectionLost = onConnectionLost;
   client.onMessageArrived = onMessageArrived;
   client.connect({onSuccess:onConnect});
-  renderStatus('Ayy it\'s lit')
-
-
-  // getCurrentTabUrl(function(url) {
-  //   renderStatus('Performing Google Image search for ' + url);
-
-  //   getImageUrl(url, function(imageUrl, width, height) {
-
-  //     renderStatus('Search term: ' + url + '\n' +
-  //         'Google image search result: ' + imageUrl);
-  //     var imageResult = document.getElementById('image-result');
-  //     // Explicitly set the width/height to minimize the number of reflows. For
-  //     // a single image, this does not matter, but if you're going to embed
-  //     // multiple external images in your page, then the absence of width/height
-  //     // attributes causes the popup to resize multiple times.
-  //     imageResult.width = width;
-  //     imageResult.height = height;
-  //     imageResult.src = imageUrl;
-  //     imageResult.hidden = false;
-
-  //   }, function(errorMessage) {
-  //     renderStatus('Cannot display image. ' + errorMessage);
-  //   });
-  // });
+  renderStatus('Welcome to CRAM - Happy Chatting!');
 });
