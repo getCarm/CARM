0d3217a7589340d19c66a2f18d234f25f8ab2741
Vansh Gandhi <vansh.gandhi@gmail.com>
2016-10-01 11:05:09 -0700

    Sending messages

--- popup.js
+++ popup.js
@@ -5,28 +5,38 @@
 // Create a client instance
 
 // called when the client connects
+var url = '' //Global URL variable
 function onConnect() {
   // Once a connection has been made, make a subscription and send a message.
-  console.log("onConnect");
-  client.subscribe("/World");
-  message = new Paho.MQTT.Message("Hello");
-  message.destinationName = "/World";
-  client.send(message);
-  alert("onConnect")
+  console.log('onConnect');
+  getCurrentTabUrl(function(u) {
+    url = u
+    client.subscribe(url)
+    console.log('Subscribed to: ' + url)
+  })
+
+  // client.subscribe("/World");
+  // message = new Paho.MQTT.Message("Hello");
+  // message.destinationName = "/World";
+  // client.send(message);
 }
 
 // called when the client loses its connection
 function onConnectionLost(responseObject) {
-  alert(responseObject.errorMessage)
   if (responseObject.errorCode !== 0) {
-    console.log("onConnectionLost:"+responseObject.errorMessage);
+    console.log('onConnectionLost: '+ responseObject.errorMessage);
   }
 }
 
 // called when a message arrives
 function onMessageArrived(message) {
-  alert(message)
-  console.log("onMessageArrived:"+message.payloadString);
+  console.log('onMessageArrived: ' + message.payloadString);
+}
+
+function sendMessage(messageText) {
+  var message = new Paho.MQTT.Message(messageText)
+  message.destinationName = url
+  client.send(message)
 }
 
 /**
@@ -50,7 +60,6 @@ function getCurrentTabUrl(callback) {
     // If you want to see the URL of other tabs (e.g. after removing active:true
     // from |queryInfo|), then the "tabs" permission is required to see their
     // "url" properties.
-    console.assert(typeof url == 'string', 'tab.url should be a string');
     callback(url);
   });
 
@@ -64,86 +73,37 @@ function getCurrentTabUrl(callback) {
   // alert(url); // Shows "undefined", because chrome.tabs.query is async.
 }
 
-/**
- * @param {string} searchTerm - Search term for Google Image search.
- * @param {function(string,number,number)} callback - Called when an image has
- *   been found. The callback gets the URL, width and height of the image.
- * @param {function(string)} errorCallback - Called when the image is not found.
- *   The callback gets a string that describes the failure reason.
- */
-function getImageUrl(searchTerm, callback, errorCallback) {
-  // Google image search - 100 searches per day.
-  // https://developers.google.com/image-search/
-  var searchUrl = 'https://ajax.googleapis.com/ajax/services/search/images' +
-    '?v=1.0&q=' + encodeURIComponent(searchTerm);
-  var x = new XMLHttpRequest();
-  x.open('GET', searchUrl);
-  // The Google image search API responds with JSON, so let Chrome parse it.
-  x.responseType = 'json';
-  x.onload = function() {
-    // Parse and process the response from Google Image Search.
-    var response = x.response;
-    if (!response || !response.responseData || !response.responseData.results ||
-        response.responseData.results.length === 0) {
-      errorCallback('No response from Google Image search!');
-      return;
-    }
-    var firstResult = response.responseData.results[0];
-    // Take the thumbnail instead of the full image to get an approximately
-    // consistent image size.
-    var imageUrl = firstResult.tbUrl;
-    var width = parseInt(firstResult.tbWidth);
-    var height = parseInt(firstResult.tbHeight);
-    console.assert(
-        typeof imageUrl == 'string' && !isNaN(width) && !isNaN(height),
-        'Unexpected respose from the Google Image Search API!');
-    callback(imageUrl, width, height);
-  };
-  x.onerror = function() {
-    errorCallback('Network error.');
-  };
-  x.send();
-}
-
 function renderStatus(statusText) {
   document.getElementById('status').textContent = statusText;
 }
 
 document.addEventListener('DOMContentLoaded', function() {
-  // var script = document.createElement('script');
-  // script.src = 'mqttws31.min.js';
-  // script.type = 'text/javascript';
-  // document.getElementsByTagName('head')[0].appendChild(script);
-  client = new Paho.MQTT.Client("broker.hivemq.com", 1883, "vansh");
-
-  // set callback handlers
+  client = new Paho.MQTT.Client('broker.hivemq.com', 8000, 'vansh');
   client.onConnectionLost = onConnectionLost;
   client.onMessageArrived = onMessageArrived;
-
-  // connect the client
   client.connect({onSuccess:onConnect});
+  renderStatus('Ayy it\'s lit')
 
 
-  getCurrentTabUrl(function(url) {
-    // Put the image URL in Google search.
-    renderStatus('Performing Google Image search for ' + url);
+  // getCurrentTabUrl(function(url) {
+  //   renderStatus('Performing Google Image search for ' + url);
 
-    getImageUrl(url, function(imageUrl, width, height) {
+  //   getImageUrl(url, function(imageUrl, width, height) {
 
-      renderStatus('Search term: ' + url + '\n' +
-          'Google image search result: ' + imageUrl);
-      var imageResult = document.getElementById('image-result');
-      // Explicitly set the width/height to minimize the number of reflows. For
-      // a single image, this does not matter, but if you're going to embed
-      // multiple external images in your page, then the absence of width/height
-      // attributes causes the popup to resize multiple times.
-      imageResult.width = width;
-      imageResult.height = height;
-      imageResult.src = imageUrl;
-      imageResult.hidden = false;
+  //     renderStatus('Search term: ' + url + '\n' +
+  //         'Google image search result: ' + imageUrl);
+  //     var imageResult = document.getElementById('image-result');
+  //     // Explicitly set the width/height to minimize the number of reflows. For
+  //     // a single image, this does not matter, but if you're going to embed
+  //     // multiple external images in your page, then the absence of width/height
+  //     // attributes causes the popup to resize multiple times.
+  //     imageResult.width = width;
+  //     imageResult.height = height;
+  //     imageResult.src = imageUrl;
+  //     imageResult.hidden = false;
 
-    }, function(errorMessage) {
-      renderStatus('Cannot display image. ' + errorMessage);
-    });
-  });
+  //   }, function(errorMessage) {
+  //     renderStatus('Cannot display image. ' + errorMessage);
+  //   });
+  // });
 });
